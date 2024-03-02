3031f752db317c2f82c56aa61987524449df6e38
cankatizm <cankatizm@gmail.com>
2024-03-02 11:32:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ var configs = (function () {
         rmdir_help: "Remove directory, this command will only work if the folders are empty.",
         touch_help: "Change file timestamps. If the file doesn't exist, it's created an empty one.",
         sudo_help: "Execute a command as the superuser.",
-        welcome: "Welcome to Cankat Aykurt's homepage (nerd and retrospective version).\nExecute the 'help' command for assistance or use more user-friendly menu at your left.",
+        welcome: "Welcome to Cankat Aykurt's homepage (nerd and retrospective version).\nExecute the 'help' command for assistance or use more user-friendly menu on your left.",
         internet_explorer_warning: "NOTE: I see you're using internet explorer, this website won't work properly.",
         welcome_file_name: "welcome_message.txt",
         invalid_command_message: "<value>: command not found.",
